770a0a17f5fbcb7c44211cd3e4023166724b250a
deepakcroy <deepak.kth@gmail.com>
2024-09-27 23:45:20 +0600

    Initial Commit

---
+++ WindowsAdhocDataGridSQLDeveloper/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace WindowsAdhocDataGridSQLDeveloper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.panel1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.panel2);
+            this.splitContainer1.Size = new System.Drawing.Size(923, 305);
+            this.splitContainer1.SplitterDistance = 118;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.panel4);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(5);
+            this.panel1.Size = new System.Drawing.Size(923, 118);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel2.Controls.Add(this.dataGridView1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Padding = new System.Windows.Forms.Padding(5);
+            this.panel2.Size = new System.Drawing.Size(923, 183);
+            this.panel2.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.InactiveCaption;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(5, 5);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(913, 173);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.flowLayoutPanel1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel3.Location = new System.Drawing.Point(5, 78);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(913, 35);
+            this.panel3.TabIndex = 0;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.textBox1);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel4.Location = new System.Drawing.Point(5, 5);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(913, 73);
+            this.panel4.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(835, 7);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 7, 3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Query";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(913, 73);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "select * from transaction";
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.button1);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(913, 35);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(923, 305);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+    }
+}
+
---
+++ WindowsAdhocDataGridSQLDeveloper/Form1.cs
@@ -0,0 +1,240 @@
+﻿using Oracle.DataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace WindowsAdhocDataGridSQLDeveloper
+{
+    public partial class Form1 : Form
+    {
+        //public static string connectionString = "Server=localhost;Port=3306;Database=tcs_dpdc_mw_prod;Uid=deepak;Pwd=821216;charset=utf8;";
+        static string host = "192.168.200.23";
+        static string port = "1521";
+        static string sid = "prepaydb.dpdc.org.bd";
+        static string user = "prepaid1";
+        static string pwd = "PrePaid132";
+        public static string connectionString = "Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST= " + host + " )(PORT= " + port + " )))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME= " + sid + " )));User Id= " + user + " ;Password=" + pwd + ";";
+
+        private List<object[]> data = new List<object[]>();
+        private int loadedRows = 0;
+        private const int batchSize = 50; // Number of rows to load at a time
+
+        private OracleConnection connection; // Class-level variable for connection
+        private OracleCommand command; // Class-level variable for command
+        private OracleDataReader reader; // Class-level variable for reader
+
+        private bool isLoading = false; // Flag to prevent multiple loads
+        private System.Timers.Timer scrollTimer; // Timer for debouncing scroll
+
+        private object _locker = new object();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.dataGridView1.VirtualMode = true;
+            this.dataGridView1.CellValueNeeded += DataGridView1_CellValueNeeded;
+            this.dataGridView1.Scroll += DataGridView1_Scroll; // Handle Scroll event
+            this.dataGridView1.RowPostPaint += DataGridView1_RowPostPaint; // Subscribe to RowPostPaint
+
+
+
+            //this.dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.DisplayedCells;
+
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = false;
+            this.dataGridView1.SelectionMode = DataGridViewSelectionMode.RowHeaderSelect;
+
+            // this.dataGridView1.SetRedraw(false);
+            this.dataGridView1.SetDoubleBuffering(true);
+
+
+
+            // Initialize the scroll timer
+            scrollTimer = new System.Timers.Timer(100); // Set debounce time (in milliseconds)
+            scrollTimer.Elapsed += OnScrollTimerElapsed;
+            scrollTimer.AutoReset = false; // Run only once
+
+            // LoadDataAsync(); // Initial load
+        }
+
+        private async void LoadDataAsync()
+        {
+            try
+            {
+                connection = new OracleConnection(connectionString);
+                await connection.OpenAsync();
+
+                // Prepare query
+                string query = textBox1.Text.Trim();
+                command = new OracleCommand(query, connection);
+                command.CommandType = CommandType.Text;
+
+                reader = (OracleDataReader)await command.ExecuteReaderAsync(CommandBehavior.Default);
+
+                // Clear previous data and columns
+                data.Clear();
+                dataGridView1.Columns.Clear();
+
+                // Create columns in the DataGridView
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    dataGridView1.Columns.Add(reader.GetName(i), reader.GetName(i));
+                }
+
+                // Load initial batch of data
+                await LoadMoreDataAsync(batchSize);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void DataGridView1_Scroll(object sender, ScrollEventArgs e)
+        {
+            // Check if the user is close to the bottom of the DataGridView
+            if (dataGridView1.FirstDisplayedScrollingRowIndex + dataGridView1.DisplayedRowCount(false) >= loadedRows - 5 && !isLoading)
+            {
+                // Start loading more data
+                _ = LoadMoreDataAsync(batchSize);  // Load more data asynchronously
+            }
+        }
+
+        private async Task LoadMoreDataAsync(int numberOfRows)
+        {
+            // Check if already loading data
+            if (isLoading) return;
+
+            isLoading = true; // Set loading flag to true
+
+            try
+            {
+                // To ensure that the data loading does not block the UI thread, consider running it in a separate task when loading more data
+                await Task.Run(async () =>
+                {
+                    // Read the next batch of data using the cursor
+                    for (int i = 0; i < numberOfRows; i++)
+                    {
+                        if (await reader.ReadAsync())
+                        {
+                            object[] rowData = new object[reader.FieldCount];
+                            reader.GetValues(rowData);
+                            data.Add(rowData);
+                            loadedRows++; // Keep track of how many rows have been loaded
+                        }
+                        else
+                        {
+                            // No more rows to read, stop loading
+                            break;
+                        }
+                    }
+                });
+
+                // Update DataGridView row count on the UI thread
+                this.Invoke((MethodInvoker)delegate
+                {
+                    dataGridView1.RowCount = loadedRows; // Set the row count to the loaded rows
+                    dataGridView1.Invalidate(); // Refresh the DataGridView to reflect changes
+                });
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            finally
+            {
+                isLoading = false; // Reset loading flag
+            }
+        }
+
+        private void DataGridView1_CellValueNeeded(object sender, DataGridViewCellValueEventArgs e)
+        {
+            if (e.RowIndex < data.Count)
+            {
+                e.Value = data[e.RowIndex][e.ColumnIndex];
+            }
+        }
+
+        private async void OnScrollTimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            // Marshal to the UI thread to check if close to the bottom of the currently loaded rows
+            this.Invoke((MethodInvoker)delegate
+            {
+                // Check if we're near the bottom
+                if (dataGridView1.FirstDisplayedScrollingRowIndex + dataGridView1.DisplayedRowCount(false) >= loadedRows - 5)
+                {
+                    isLoading = true; // Set loading flag
+                }
+            });
+
+            // Proceed with loading the next batch of data asynchronously outside of the Invoke
+            if (isLoading)
+            {
+                int nextBatchSize = Math.Min(batchSize, 50);
+                await LoadMoreDataAsync(nextBatchSize);
+
+                // Update isLoading flag back on the UI thread after async operation completes
+                this.Invoke((MethodInvoker)delegate
+                {
+                    isLoading = false; // Reset loading flag
+                });
+            }
+        }
+
+        private void ResetDataGridView()
+        {
+            // Clear existing data and columns
+            data.Clear();
+            loadedRows = 0; // Reset loaded rows count
+
+            // Clear the DataGridView
+            dataGridView1.Rows.Clear();
+            dataGridView1.Columns.Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ResetDataGridView();
+            LoadDataAsync(); // Reload data on button click
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            base.OnFormClosing(e);
+            // Close the reader and connection when the form is closing
+            if (reader != null && !reader.IsClosed)
+            {
+                reader.Close();
+            }
+            if (command != null)
+            {
+                command.Dispose();
+            }
+            if (connection != null)
+            {
+                connection.Close();
+            }
+        }
+        private void DataGridView1_RowPostPaint(object sender, DataGridViewRowPostPaintEventArgs e)
+        {
+            // Draw row number
+            int rowIndex = e.RowIndex + 1; // 1-based index
+            Rectangle headerBounds = new Rectangle(e.RowBounds.Left, e.RowBounds.Top, e.RowBounds.Width, e.RowBounds.Height);
+
+            using (Brush brush = new SolidBrush(dataGridView1.ForeColor))
+            {
+                e.Graphics.DrawString(rowIndex.ToString(), dataGridView1.Font, brush, headerBounds);
+            }
+        }
+
+    }
+}
